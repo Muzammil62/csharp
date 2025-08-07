6a1ee952c3e4b84a5d9c4174985044bb0fba14ec
Muzammil62 <ibrahimmuzammil62@gmail.com>
2025-08-07 14:46:16 +0530

    Add files via upload

---
+++ person.cs
@@ -0,0 +1,28 @@
+﻿namespace new_project
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Age must be between 1 and 100.");
+                }
+            }
+        }
+    }
+}
