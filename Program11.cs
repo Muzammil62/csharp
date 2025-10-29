b81ab59b84008db7dc3ccf3b676e93c3fbe8fd0a
Muzammil62 <ibrahimmuzammil62@gmail.com>
2025-10-29 22:37:49 +0400

    Add vehicle interface and implementations
    Implemented IVehicle interface with Car, Bike, and Bus classes.

---
+++ Program11.cs
@@ -0,0 +1,65 @@
+using System;
+
+interface IVehicle
+{
+    void Start();
+    void Stop();
+}
+
+class Car : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Car started.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Car stopped.");
+    }
+}
+
+class Bike : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Bike started.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Bike stopped.");
+    }
+}
+
+class Bus : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Bus started.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Bus stopped.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IVehicle car = new Car();
+        IVehicle bike = new Bike();
+        IVehicle bus = new Bus();
+
+        car.Start();
+        car.Stop();
+
+        bike.Start();
+        bike.Stop();
+
+        bus.Start();
+        bus.Stop();
+    }
+}
