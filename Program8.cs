59940eb4403d40e1fc25398dab2255acc6b81b50
Muzammil62 <ibrahimmuzammil62@gmail.com>
2025-10-29 22:31:42 +0400

    Implement square number calculation program

---
+++ Program8.cs
@@ -0,0 +1,27 @@
+using System;
+
+class SquareNumber
+{
+    int num;
+
+    public void ReadNumber()
+    {
+        Console.Write("Enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void DisplaySquare()
+    {
+        Console.WriteLine("Square of " + num + " is: " + (num * num));
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SquareNumber obj = new SquareNumber();
+        obj.ReadNumber();
+        obj.DisplaySquare();
+    }
+}
